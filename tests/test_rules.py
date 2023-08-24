8e9cd465b1ff8a65be72024196dfd615bf2ab3cd
Joshua Hampton <joshua.hampton@live.co.uk>
2023-08-24 15:34:40 +0100

    Added more regex-rules and some rule-func tests

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -57,5 +57,44 @@ def test_regex_rules():
     assert r.check(rule, "@amail.com") != []
     assert r.check(rule, "freda.bloggs@") != []
 
+    rule = "regex-rule:valid-url"
+    assert r.check(rule, "https://github.com/") == []
+    assert r.check(rule, "https://") != []
+    assert r.check(rule, "github.com") != []
+
+    rule = "regex-rule:match:vN.M"
+    assert r.check(rule, "v1.0") == []
+    assert r.check(rule, "1.0") != []
+    assert r.check(rule, "v1") != []
+
+    rule = "regex-rule:number"
+    assert r.check(rule, "42") == []
+    assert r.check(rule, "-42") == []
+    assert r.check(rule, "forty two") != []
+
+def test_rule_funcs():
+    rule = "rule-func:match-one-of:butcher|baker|candle stick maker"
+    assert r.check(rule, "butcher") == []
+    assert r.check(rule, "candle stick maker") == []
+    assert r.check(rule, "butcher's son") != []
+    assert r.check(rule, "butcher, baker") != []
+
+    rule = "rule-func:match-one-or-more-of:butcher|baker|candle stick maker"
+    assert r.check(rule, "butcher") == []
+    assert r.check(rule, "candle stick maker") == []
+    assert r.check(rule, "butcher's son") != []
+    assert r.check(rule, "butcher, baker") == []
+    assert r.check(rule, "butcher, butcher's son") != []
+
+    rule = "rule-func:string-of-length:8"
+    assert r.check(rule, "checksit") == []
+    assert r.check(rule, "check") != []
+    assert r.check(rule, "checksit check") != []
+
+    rule = "rule-func:string-of-length:8+"
+    assert r.check(rule, "checksit") == []
+    assert r.check(rule, "check") != []
+    assert r.check(rule, "checksit check") == []
+
 #TODO: Add checks for all the published rules 
 #TODO: Add checks for some regular expressions to check they are executed correctly
