16307ea69b81b3f4aba3fe30ca7ec4ea7aa9b7b2
Joshua Hampton <joshua.hampton@live.co.uk>
2023-08-25 10:34:41 +0100

    Parametrized function

--- tests/test_checksit.py
+++ tests/test_checksit.py
@@ -13,28 +13,30 @@ from click.testing import CliRunner
 from checksit import cli
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
+#@pytest.fixture
+#def response():
+#    """Sample pytest fixture.
+#
+#    See more at: http://doc.pytest.org/en/latest/fixture.html
+#    """
+#    # import requests
+#    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+#
+#
+#def test_content(response):
+#    """Sample pytest test function with the pytest fixture as an argument."""
+#    # from bs4 import BeautifulSoup
+#    # assert 'GitHub' in BeautifulSoup(response.content).title.string
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+params = [(cli.main, 0,  "Commands:\n  check\n  describe\n  show-specs\n  summary\n"), (cli.check, 2, "Error: Missing argument 'FILE_PATH'.\n")]
 
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
+@pytest.mark.parametrize("command, exit_code, output_ending", params)
+def test_command_line_interface(command, exit_code, output_ending):
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'checksit.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    result = runner.invoke(command)
+    assert result.exit_code == exit_code
+    assert result.output.endswith(output_ending) == True
+    #help_result = runner.invoke(cli.main, ['--help'])
+    #assert help_result.exit_code == 0
+    #assert '--help  Show this message and exit.' in help_result.output
