d3987fb98e6d0a4e22aa8c242eeeaf6b2e379f6e
Joshua Hampton <joshua.hampton@live.co.uk>
2024-01-11 11:31:06 +0000

    Troubleshooting - checking directory

--- tests/test_images.py
+++ tests/test_images.py
@@ -1,6 +1,12 @@
 import pytest
 from click.testing import CliRunner
 from checksit import cli
+import os
+
+# quick test to make sure python is where I think it is
+def test_where():
+    print(os.listdir("tests"))
+    assert 1 == 1
 
 
 # photos from ncas named instruments
