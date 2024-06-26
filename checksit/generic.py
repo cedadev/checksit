a7ed5cdc718d62a82b8f77bfd91defea00fb8349
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2024-06-26 15:01:22 +0100

    Add missing attr check type conversion for defined attrs of required vars

--- checksit/generic.py
+++ checksit/generic.py
@@ -363,6 +363,9 @@ def check_var(dct, variable, defined_attrs, rules_attrs=None, skip_spellcheck=Fa
             )
         else:
             for attr in defined_attrs:
+                if isinstance(attr, dict) and len(attr.keys()) == 1:
+                    for key, value in attr.items():
+                        attr = f"{key}:{value}"
                 attr_key = attr.split(":")[0]
                 attr_value = ":".join(attr.split(":")[1:])
                 if attr_key not in dct["variables"][variable]:
