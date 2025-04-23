25e08cd34631a5b846f88ce359ed95ac7e7b7de7
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2025-04-23 15:51:48 +0100

    Add check-files command

--- checksit/cli.py
+++ checksit/cli.py
@@ -7,7 +7,7 @@ __license__ = "BSD - see LICENSE file in top-level package directory"
 
 import click
 import os
-from typing import Optional, List
+from typing import Optional, List, Tuple
 
 from .utils import string_to_dict, string_to_list
 from .check import check_file
@@ -20,6 +20,112 @@ from . import specs
 def main():
     """Console script for checker."""
     return 0
+
+
+@main.command()
+@click.argument("file_paths", nargs=-1)
+@click.option("-m", "--mappings", default=None)
+@click.option("-r", "--rules", default=None)
+@click.option("-s", "--specs", default=None)
+@click.option("-i", "--ignore-attrs", default=None)
+@click.option("-G", "--ignore-all-globals", default=False)
+@click.option("-D", "--ignore-all-dimensions", default=False)
+@click.option("-V", "--ignore-all-variables", default=False)
+@click.option("-A", "--ignore-all-variable-attrs", default=False)
+@click.option("--auto-cache/--no-auto-cache", default=False)
+@click.option("-l", "--log-mode", default="standard")
+@click.option("-v", "--verbose/--no-verbose", default=False)
+@click.option("-t", "--template", default="auto")
+@click.option("-w", "--ignore-warnings", is_flag=True)
+@click.option("-p", "--skip-spellcheck", is_flag=True)
+def check_files(
+    file_paths: Tuple[str,...],
+    mappings: Optional[str] = None,
+    rules: Optional[str] = None,
+    specs: Optional[str] = None,
+    ignore_attrs: Optional[str] = None,
+    ignore_all_globals: bool = False,
+    ignore_all_dimensions: bool = False,
+    ignore_all_variables: bool = False,
+    ignore_all_variable_attrs: bool = False,
+    auto_cache: bool = False,
+    log_mode: str = "standard",
+    verbose: bool = False,
+    template: str = "auto",
+    ignore_warnings: bool = False,
+    skip_spellcheck: bool = False,
+):
+    """CLI call to check a number of files against a set of rules, specs or templates.
+
+    Reads options from the command line and calls the check_file function to check the
+    compliance of each file against a set of rules, specs or templates. If any of
+    `mappings`, `rules`, `specs` or `ignore_attrs` are provided as a string, they will
+    be converted to the appropriate data type.
+
+    Args:
+        file_paths: Paths to the files to check.
+        mappings: Map variable names between name used in file and name in template.
+          Format should be `<template variable name>=<file variable name>`. Multiple
+          mappings should be separated by a comma.
+        rules: Specific rules to use to check items. Format should be
+          `<what to check>=<rule type>:<function/check>[:<extras>[:<extras>...]]`.
+          Multiple rules should be separated by a comma.
+        specs: Specific specs to use to check items. Format should be `<spec file>` or
+          `<spec folder>/<spec file>`. File location and path relative to the specs
+          folder in the checksit repository. Multiple specs should be separated by a
+          comma.
+        ignore_attrs: Attributes to ignore when checking variables. Multiple attributes
+          should be separated by a comma.
+        ignore_all_globals: Not implemented yet.
+        ignore_all_dimensions: Not implemented yet.
+        ignore_all_variables: Not implemented yet.
+        ignore_all_variable_attrs: Not implemented yet.
+        auto_cache: Store the file in the template cache for future use as a template.
+        log_mode: How the output should be printed. Options are "standard" (default)
+          and "compact".
+        verbose: Print additional information to the console.
+        template: Template to use for checking. Options are "auto" (default), "off", or
+          `<template file>`. File location is relative to the top level of the checksit
+          repository.
+        ignore_warnings: Ignore warnings when checking the file, only return errors.
+        skip_spellcheck: Skip the spellcheck in rules and functions that utilise spell
+          checking.
+    """
+
+    if (
+        ignore_all_globals
+        or ignore_all_dimensions
+        or ignore_all_variables
+        or ignore_all_variable_attrs
+    ):
+        raise Exception("Options not implemented yet!!!!!")
+
+    if mappings is not None:
+        mappings = string_to_dict(mappings)
+
+    if rules is not None:
+        rules = string_to_dict(rules)
+
+    if specs is not None:
+        specs = string_to_list(specs)
+
+    if ignore_attrs is not None:
+        ignore_attrs = string_to_list(ignore_attrs)
+
+    for file_path in file_paths:
+        check_file(
+            file_path,
+            template=template,
+            mappings=mappings,
+            extra_rules=rules,
+            specs=specs,
+            ignore_attrs=ignore_attrs,
+            auto_cache=auto_cache,
+            verbose=verbose,
+            log_mode=log_mode,
+            ignore_warnings=ignore_warnings,
+            skip_spellcheck=skip_spellcheck,
+        )
 
 
 @main.command()
