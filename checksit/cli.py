e3d03a2d2d92823277b5893237eca40d8db44580
Joshua Hampton <joshua.hampton@ncas.ac.uk>
2026-03-16 14:37:44 +0000

    Add -d/--debug flag option

--- checksit/cli.py
+++ checksit/cli.py
@@ -7,6 +7,7 @@ __license__ = "BSD - see LICENSE file in top-level package directory"
 
 import click
 import os
+import sys
 from typing import Optional, List, Tuple
 
 from .utils import string_to_dict, string_to_list
@@ -16,10 +17,33 @@ from . import describer
 from . import specs
 
 
+class CliConfig:
+    def __init__(self, debug=False):
+        self.debug = debug
+
+    def handle_error(self, exception):
+        if self.debug:
+            raise exception
+        else:
+            click.echo(
+                (
+                    "[ERROR] - checksit encountered an unexpected error and quit. Run"
+                    " 'checksit --debug <command> <options>' for full details."
+                ),
+                err=True
+            )
+            sys.exit(1)
+
+pass_config = click.make_pass_decorator(CliConfig, ensure=True)
+
+
 @click.group()
-def main():
+@click.option('--debug', '-d', is_flag=True, default=False, help="Enable debug mode.")
+@click.pass_context
+def main(ctx, debug):
     """Console script for checker."""
-    return 0
+    #return 0
+    ctx.obj = CliConfig(debug)
 
 
 @main.command()
@@ -38,7 +62,9 @@ def main():
 @click.option("-t", "--template", default="auto")
 @click.option("-w", "--ignore-warnings", is_flag=True)
 @click.option("-p", "--skip-spellcheck", is_flag=True)
+@pass_config
 def check_files(
+    config: CliConfig,
     file_paths: Tuple[str,...],
     mappings: Optional[str] = None,
     rules: Optional[str] = None,
@@ -91,42 +117,43 @@ def check_files(
         skip_spellcheck: Skip the spellcheck in rules and functions that utilise spell
           checking.
     """
+    try:
+        if (
+            ignore_all_globals
+            or ignore_all_dimensions
+            or ignore_all_variables
+            or ignore_all_variable_attrs
+        ):
+            raise Exception("Options not implemented yet!!!!!")
 
-    if (
-        ignore_all_globals
-        or ignore_all_dimensions
-        or ignore_all_variables
-        or ignore_all_variable_attrs
-    ):
-        raise Exception("Options not implemented yet!!!!!")
+        if mappings is not None:
+            mappings = string_to_dict(mappings)
 
-    if mappings is not None:
-        mappings = string_to_dict(mappings)
+        if rules is not None:
+            rules = string_to_dict(rules)
 
-    if rules is not None:
-        rules = string_to_dict(rules)
+        if specs is not None:
+            specs = string_to_list(specs)
 
-    if specs is not None:
-        specs = string_to_list(specs)
+        if ignore_attrs is not None:
+            ignore_attrs = string_to_list(ignore_attrs)
 
-    if ignore_attrs is not None:
-        ignore_attrs = string_to_list(ignore_attrs)
-
-    for file_path in file_paths:
-        check_file(
-            file_path,
-            template=template,
-            mappings=mappings,
-            extra_rules=rules,
-            specs=specs,
-            ignore_attrs=ignore_attrs,
-            auto_cache=auto_cache,
-            verbose=verbose,
-            log_mode=log_mode,
-            ignore_warnings=ignore_warnings,
-            skip_spellcheck=skip_spellcheck,
-        )
-
+        for file_path in file_paths:
+            check_file(
+                file_path,
+                template=template,
+                mappings=mappings,
+                extra_rules=rules,
+                specs=specs,
+                ignore_attrs=ignore_attrs,
+                auto_cache=auto_cache,
+                verbose=verbose,
+                log_mode=log_mode,
+                ignore_warnings=ignore_warnings,
+                skip_spellcheck=skip_spellcheck,
+            )
+    except Exception as e:
+        config.handle_error(e)
 
 @main.command()
 @click.argument("file_path")
@@ -144,7 +171,9 @@ def check_files(
 @click.option("-t", "--template", default="auto")
 @click.option("-w", "--ignore-warnings", is_flag=True)
 @click.option("-p", "--skip-spellcheck", is_flag=True)
+@pass_config
 def check(
+    config: CliConfig,
     file_path: str,
     mappings: Optional[str] = None,
     rules: Optional[str] = None,
@@ -197,40 +226,42 @@ def check(
         skip_spellcheck: Skip the spellcheck in rules and functions that utilise spell
           checking.
     """
+    try:
+        if (
+            ignore_all_globals
+            or ignore_all_dimensions
+            or ignore_all_variables
+            or ignore_all_variable_attrs
+        ):
+            raise Exception("Options not implemented yet!!!!!")
 
-    if (
-        ignore_all_globals
-        or ignore_all_dimensions
-        or ignore_all_variables
-        or ignore_all_variable_attrs
-    ):
-        raise Exception("Options not implemented yet!!!!!")
+        if mappings is not None:
+            mappings = string_to_dict(mappings)
 
-    if mappings is not None:
-        mappings = string_to_dict(mappings)
+        if rules is not None:
+            rules = string_to_dict(rules)
 
-    if rules is not None:
-        rules = string_to_dict(rules)
+        if specs is not None:
+            specs = string_to_list(specs)
 
-    if specs is not None:
-        specs = string_to_list(specs)
+        if ignore_attrs is not None:
+            ignore_attrs = string_to_list(ignore_attrs)
 
-    if ignore_attrs is not None:
-        ignore_attrs = string_to_list(ignore_attrs)
-
-    check_file(
-        file_path,
-        template=template,
-        mappings=mappings,
-        extra_rules=rules,
-        specs=specs,
-        ignore_attrs=ignore_attrs,
-        auto_cache=auto_cache,
-        verbose=verbose,
-        log_mode=log_mode,
-        ignore_warnings=ignore_warnings,
-        skip_spellcheck=skip_spellcheck,
-    )
+        check_file(
+            file_path,
+            template=template,
+            mappings=mappings,
+            extra_rules=rules,
+            specs=specs,
+            ignore_attrs=ignore_attrs,
+            auto_cache=auto_cache,
+            verbose=verbose,
+            log_mode=log_mode,
+            ignore_warnings=ignore_warnings,
+            skip_spellcheck=skip_spellcheck,
+        )
+    except Exception as e:
+        config.handle_error(e)
 
 
 @main.command()
@@ -240,7 +271,9 @@ def check(
 @click.option("-x", "--exclude", default=None)
 @click.option("-e", "--exclude-file", default=None)
 @click.option("--verbose/--no-verbose", default=False)
+@pass_config
 def summary(
+    config: CliConfig,
     log_files: Optional[List[str]] = None,
     log_directory: Optional[str] = None,
     show_files: bool = False,
@@ -265,38 +298,41 @@ def summary(
           should be on a new line.
         verbose: Print additional information to the console.
     """
+    try:
+        if exclude is not None:
+            exclude = string_to_list(exclude)
+        else:
+            exclude = []
 
-    if exclude is not None:
-        exclude = string_to_list(exclude)
-    else:
-        exclude = []
+        if exclude_file:
+            if not os.path.isfile(exclude_file):
+                raise Exception(f"'--exclude-file' does not point to a valid file")
 
-    if exclude_file:
-        if not os.path.isfile(exclude_file):
-            raise Exception(f"'--exclude-file' does not point to a valid file")
+            with open(exclude_file) as exfile:
+                exclude.extend(
+                    [
+                        exclude_pattern
+                        for exclude_pattern in exfile
+                        if exclude_pattern.strip()
+                    ]
+                )
 
-        with open(exclude_file) as exfile:
-            exclude.extend(
-                [
-                    exclude_pattern
-                    for exclude_pattern in exfile
-                    if exclude_pattern.strip()
-                ]
-            )
-
-    summarise(
-        log_files,
-        log_directory=log_directory,
-        show_files=show_files,
-        exclude=exclude,
-        verbose=verbose,
-    )
+        summarise(
+            log_files,
+            log_directory=log_directory,
+            show_files=show_files,
+            exclude=exclude,
+            verbose=verbose,
+        )
+    except Exception as e:
+        config.handle_error(e)
 
 
 @main.command()
 @click.argument("check_ids", nargs=-1, default=None)
 @click.option("--verbose/--no-verbose", default=False)
-def describe(check_ids: Optional[List[str]] = None, verbose: bool = False):
+@pass_config
+def describe(config: CliConfig, check_ids: Optional[List[str]] = None, verbose: bool = False):
     """CLI call to describe rules.
 
     Reads options from the command line and calls the describe function to print the
@@ -307,12 +343,16 @@ def describe(check_ids: Optional[List[str]] = None, verbose: bool = False):
         check_ids: List of rules to describe.
         verbose: Print additional information to the console.
     """
-    describer.describe(check_ids, verbose=verbose)
+    try:
+        describer.describe(check_ids, verbose=verbose)
+    except Exception as e:
+        config.handle_error(e)
 
 
 @main.command()
 @click.argument("spec_ids", nargs=-1, default=None)
-def show_specs(spec_ids: Optional[List[str]] = None):
+@pass_config
+def show_specs(config: CliConfig, spec_ids: Optional[List[str]] = None):
     """CLI call to show details of specs.
 
     Reads options from the command line and calls the show_specs function to print the
@@ -322,7 +362,10 @@ def show_specs(spec_ids: Optional[List[str]] = None):
     Args:
         spec_ids: List of specs to show.
     """
-    specs.show_specs(spec_ids)
+    try:
+        specs.show_specs(spec_ids)
+    except Exception as e:
+        config.handle_error(e)
 
 
 if __name__ == "__main__":
